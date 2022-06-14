e569b97fa81afd1f15ac6308ef16ec81c2891b73
Kristina <krislady1412@gmail.com>
2022-06-14 15:28:51 +0600

    Random

---
+++ Example1_HelloConsole/Example3_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿// int numberA = 5;
+// int numberB = 3;
+// Console.WriteLine (numberA+numberB);
+int numberA = new Random() .Next(1, 10); //1,2,3...9
+Console.WriteLine(numberA);
+int numberB = new Random() .Next(1, 10); //1,2,3...9
+Console.WriteLine(numberB);
+Console.WriteLine (numberA+numberB);
